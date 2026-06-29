ce0f93f247eaca28c1149cbda53d9ca9664caf7c
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-06-29 23:57:10 +0600

    Solved palindrome using two pointer pattern

---
+++ cp11_check_if_array_contains_duplicates.py
@@ -0,0 +1,21 @@
+"""Check if array contains duplicates"""
+# Return True if any value appears at least twice in the give array
+# otherwise false
+# Constraints, don't use nums.count or collections.counter
+nums = [1, 2, 3, 1]
+
+def check_duplicates(numbers):
+
+    seen = set() # Using set because we only need to remember the seen number. Dictionary can be used too
+
+    for num in numbers:
+
+        if num in seen:
+           return True
+
+        else:
+            seen.add(num)
+    return False
+
+if __name__ == '__main__':
+    print(check_duplicates(nums))
---
+++ cp12_two_pointers_for_checking_palindrome_string.py
@@ -0,0 +1,20 @@
+"""Use Two pointers pattern to determine whether it is a palindrome"""
+# Examples:
+# "madam" -> true
+# "racecar" -> true
+# Constraint don't reverse the string text[::-1]
+# Compare first vs last then second first vs second last and so on
+# racecar ->   racecar
+# ^     ^       ^   ^
+def is_palindrome(chk_str):
+
+    str_len = len(chk_str)
+    for i in range(str_len // 2):
+        if chk_str[i] != chk_str[-1 - i]:
+            return False # if first and last doesn't match then break the loop and return false
+
+    return True # if all matches and makes it here return true
+
+if __name__ == '__main__':
+    print(is_palindrome('madam'))
+
