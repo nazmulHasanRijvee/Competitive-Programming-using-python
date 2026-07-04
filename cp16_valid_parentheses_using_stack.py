78faa3aa07784043a9cd9e3244ccc212a4ecefff
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-07-04 22:55:46 +0600

    Solved Valid Parentheses problem using Stack and Hashmap

---
+++ cp16_valid_parentheses_using_stack.py
@@ -0,0 +1,29 @@
+"""Determine if Parentheses are Valid Using Stack"""
+# Example: s = '(){}[]' -> true
+# s = '{[]}' -> true and s = '(]' -> false
+def is_valid_parentheses(s):
+    stack = []
+    pairs = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '['
+    }
+    for ch in s:
+        if ch in '({[':
+            stack.append(ch)
+        else:
+            if len(stack) == 0:
+                return False
+            recent = stack.pop()
+
+            # using .get() to handle the edge case if abc is
+            # given instead of () {} [] parentheses the dictionary
+            # will throw error, Key not found
+            if pairs.get(ch, '0') != recent:
+                return False
+
+    return len(stack) == 0
+
+
+if __name__ == "__main__" :
+    print(is_valid_parentheses("()[]"))
